b3e4c6f6e54eeb6c2e31ef40b7ea1186e9bcc65f
Federico Berto <Juju-botu>
2021-08-06 00:55:48 +0900

    Add exclusions

--- kakao/config.py
+++ kakao/config.py
@@ -37,15 +37,39 @@ def input_config():
         {"name": "(미사용)", "code": "VEN00020"},
     ]
     vaccine_type = None
+    exclusions = []
     while True:
+        # Select the vaccine
         print("=== Vaccine List ===")
         for vaccine in vaccine_candidates:
             if vaccine["name"] == "(미사용)":
                 continue
             print(
                 f"{fill_str_with_space(vaccine['name'], 10)} : {vaccine['code']}")
-
+        print("[Note: you can select ANY and exclude some vaccines later]")
         vaccine_type = str.upper(input("Please enter the vaccine code: ").strip())
+        
+        # Select exclusions from "ANY" method
+        if vaccine_type == "ANY":
+            while True:
+                print("=== Selected Vaccines List ===")
+                for vaccine in vaccine_candidates:
+                    if vaccine["name"] == "(미사용)" or vaccine["code"] == "ANY" or vaccine["code"] in exclusions:
+                        continue
+                    print(f"{fill_str_with_space(vaccine['name'], 10)} : {vaccine['code']}")
+                exclusion = str.upper(input("Please enter the code of the vaccine you want to exclude or enter N to exit: ").strip())
+                if exclusion == "N":
+                    break
+                if any(x["code"] == exclusion for x in vaccine_candidates):
+                    if exclusion == "ANY":
+                        print("ANY is not a valid input")
+                    elif exclusion not in exclusions:
+                        exclusions.append(exclusion)
+                    else:
+                        print("Vaccine was already added to exclusions!")
+                else:
+                    print("Unknown vaccine code inserted, please check!")
+                
         if any(x["code"] == vaccine_type for x in vaccine_candidates) or vaccine_type.startswith("FORCE:"):
             if vaccine_type.startswith("FORCE:"):
                 vaccine_type = vaccine_type[6:]
@@ -73,7 +97,7 @@ def input_config():
 
             break
         else:
-            print("Please check the vaccine code.")
+            print("Unknown vaccine code inserted, please check!")
 
     print("After you specify a range of vaccines in a square shape," +
           " check the vaccine within that range and try to make an appointment with the hospital if there are any remaining vaccines.")
@@ -81,7 +105,7 @@ def input_config():
     top_x = None
     top_y = None
     while top_x is None or top_y is None:
-        top_y, top_x = input("Please enter the top left coordination. 36.32346792518988, 127.46080403718007: ").strip().split(",")
+        top_y, top_x = input("Please enter the top left coordinates (Copy these for Daejeon: 36.32346792518988, 127.46080403718007):").strip().split(",")
         if not is_in_range(coord_type="x", coord=top_x) or not is_in_range(coord_type="y", coord=top_y):
             print(f"This is not a valid coordinate value. Input Value : {top_y}, {top_x}")
             top_x = None
@@ -93,7 +117,7 @@ def input_config():
     bottom_x = None
     bottom_y = None
     while bottom_x is None or bottom_y is None:
-        bottom_y, bottom_x = input("Please enter the bottom right coordination. 36.431638008998874, 127.2857236995207: ").strip().split(",")
+        bottom_y, bottom_x = input("Please enter the bottom right coordinates (Copy these for Daejeon: 36.431638008998874, 127.2857236995207): ").strip().split(",")
         if not is_in_range(coord_type="x", coord=bottom_x) or not is_in_range(coord_type="y", coord=bottom_y):
             print(f"This is not a valid coordinate value. Input Value : {bottom_y}, {bottom_x}")
             bottom_x = None
@@ -113,8 +137,8 @@ def input_config():
             print("Please enter Y or N.")
             only_left = None
 
-    dump_config(vaccine_type, top_x, top_y, bottom_x, bottom_y, only_left)
-    return vaccine_type, top_x, top_y, bottom_x, bottom_y, only_left
+    dump_config(vaccine_type, top_x, top_y, bottom_x, bottom_y, only_left, exclusions)
+    return vaccine_type, top_x, top_y, bottom_x, bottom_y, only_left, exclusions
 
 
 # 기존 입력 값 로딩
@@ -144,18 +168,19 @@ def load_config():
                     previous_bottom_x = configuration["botX"]
                     previous_bottom_y = configuration["botY"]
                     previous_only_left = configuration["onlyLeft"] == "True"
-                    return previous_used_type, previous_top_x, previous_top_y, previous_bottom_x, previous_bottom_y, previous_only_left
+                    previous_exclusions = configuration["exclusions"].split(',')
+                    return previous_used_type, previous_top_x, previous_top_y, previous_bottom_x, previous_bottom_y, previous_only_left, previous_exclusions
                 except KeyError:
                     print('No information was previously entered.')
             else:
-                return None, None, None, None, None, None
+                return None, None, None, None, None, None, None
         except ValueError:
-            return None, None, None, None, None, None
-    return None, None, None, None, None, None
+            return None, None, None, None, None, None, None
+    return None, None, None, None, None, None, None
 
 
 # pylint: disable=too-many-arguments
-def dump_config(vaccine_type, top_x, top_y, bottom_x, bottom_y, only_left, search_time=0.1):
+def dump_config(vaccine_type, top_x, top_y, bottom_x, bottom_y, only_left, exclusions, search_time=0.1):
     config_parser = configparser.ConfigParser()
     config_parser['config'] = {}
     conf = config_parser['config']
@@ -166,7 +191,8 @@ def dump_config(vaccine_type, top_x, top_y, bottom_x, bottom_y, only_left, searc
     conf["botY"] = bottom_y
     conf["search_time"] = str(search_time)
     conf["onlyLeft"] = "True" if only_left else "False"
-
+    conf["exclusions"] = ",".join(exclusions) # list into comma separated variables
+    
     with open("config.ini", "w") as config_file:
         config_parser.write(config_file)
 
